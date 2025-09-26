dde075f891c569c1fdd9e33274bfad98bc73ac47
gustavocaruso1 <gustavocaruso82@gmail.com>
2025-09-25 22:30:06 -0300

    Iniciado a estilização do Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <>
+        <Navbar/>
+      </>
     </div>
   )
 }
